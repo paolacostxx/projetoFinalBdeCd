b4f841af0932084feabaf8ff334f6e77147891ca
Lauren Reis <laurenjuliareissilva@hotmail.com>
2025-06-16 21:47:12 -0300

    Update dql.sql

--- dql.sql
+++ dql.sql
@@ -4,12 +4,6 @@ FROM agendamento a
 INNER JOIN clienteSalao c ON a.cli_id = c.cli_id
 INNER JOIN funcionario f ON a.id_funcionario = f.id_funcionario;
 
---  Ver os serviços de um agendamento específico
-SELECT a.id_agendamento, s.nome_servico, s.preco
-FROM agendamento_servico ag
-INNER JOIN servico s ON ag.id_servico = s.id_servico
-INNER JOIN agendamento a ON ag.id_agendamento = a.id_agendamento
-WHERE a.id_agendamento = 1;
 
 --  Ver agendamentos futuros
 SELECT * FROM agendamento
@@ -18,16 +12,9 @@ WHERE data_hora > CURRENT_TIMESTAMP;
 --  Clientes do tipo Pessoa Jurídica
 SELECT c.nome, pj.cnpj, pj.razao_social
 FROM clienteSalao c
-INNER JOIN pessoa_juridica pj ON c.cli_id = pj.cli_id;
+INNER JOIN pessoa_juridica pj ON c.cli_id = pj.id_cliente;
 
 --  Clientes do tipo Pessoa Física
 SELECT c.nome, pf.cpf, pf.data_nascimento
 FROM clienteSalao c
 INNER JOIN pessoa_fisica pf ON c.cli_id = pf.id_cliente;
-
--- 🔎 Consultar faturamento (total de serviços concluídos)
-SELECT SUM(s.preco) AS total_faturado
-FROM agendamento a
-INNER JOIN agendamento_servico ags ON a.id_agendamento = ags.id_agendamento
-INNER JOIN servico s ON ags.id_servico = s.id_servico
-WHERE a.status = 'Concluído';
